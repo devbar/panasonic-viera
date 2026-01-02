778b8163edee71ba2b88879d1f4866b6b94a28c3
devbar <github@devbar.de>
2026-01-02 16:15:28 +0100

    Adding dedicated tag for image

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker build -t registry.leviathan.lan/panasonic-viera-mqtt:latest .
+d=$(date '+%Y%m%d%H%M%S')
+
+docker build -t registry.leviathan.lan/panasonic-viera-mqtt:latest -t registry.leviathan.lan/panasonic-viera-mqtt:$d .
 docker push registry.leviathan.lan/panasonic-viera-mqtt:latest
+docker push registry.leviathan.lan/panasonic-viera-mqtt:$d
